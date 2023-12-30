73a2c046a5037ffb24dbc7131aa978c7e008fd6b
peterSanyo <peter.sanyo@code.berlin>
2023-12-31 00:53:53 +0100

    test

---
+++ RecordTest Watch App/Views/HomeView/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RecordTest Watch App
+//
+//  Created by Péter Sanyó on 31.12.23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
